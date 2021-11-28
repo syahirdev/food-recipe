75dee6e7143867d87466d1ec2672f67968143734
Syahir <67471898+Syahirdev@users.noreply.github.com>
2021-11-28 20:39:22 +0800

    insert dummy data

---
+++ client/src/assets/icons.ts
@@ -0,0 +1,41 @@
+const anchovy = require("./icons/anchovy.png")
+const cheese = require("./icons/cheese.png")
+const chicken = require("./icons/chicken.png")
+const chilli = require("./icons/chilli.png")
+const coriander = require("./icons/coriander.png")
+const egg = require("./icons/egg.png")
+const garlic = require("./icons/garlic.png")
+const lemongrass = require("./icons/lemongrass.png")
+const oil = require("./icons/oil.png")
+const onion = require("./icons/onion.png")
+const pasta = require("./icons/pasta.png")
+const pepper = require("./icons/pepper.png")
+const potato = require("./icons/potato.png")
+const rice = require("./icons/rice.png")
+const shallot = require("./icons/shallot.png")
+const salt = require("./icons/salt.png")
+const sugar = require("./icons/sugar.png")
+const shrimp = require("./icons/shrimp.png")
+const tomato = require("./icons/tomato.png")
+
+export default {
+    anchovy,
+    coriander,
+    chicken,
+    chilli,
+    cheese,
+    egg,
+    garlic,
+    lemongrass,
+    onion,
+    potato,
+    rice,
+    shrimp,
+    shallot,
+    tomato,
+    oil,
+    pasta,
+    pepper,
+    salt,
+    sugar,
+}
---
+++ client/src/assets/images.js
@@ -0,0 +1,40 @@
+const loginBackground = require('./images/system/login-background.png');
+
+const recipe = require('./images/recipes/recipe.png');
+const spagetti = require('./images/recipes/spagetti.png');
+const satay = require('./images/recipes/satay.png');
+const laksa = require('./images/recipes/laksa.png');
+const nasiLemak = require('./images/recipes/nasi-lemak.webp');
+
+const profile = require('./images/dummy_profiles/profile.png');
+const UserProfile1 = require('./images/dummy_profiles/profile-pic-1.png');
+const UserProfile2 = require('./images/dummy_profiles/profile-pic-2.png');
+const UserProfile3 = require('./images/dummy_profiles/profile-pic-3.png');
+const UserProfile4 = require('./images/dummy_profiles/profile-pic-4.png');
+const UserProfile5 = require('./images/dummy_profiles/profile-pic-5.png');
+const UserProfile6 = require('./images/dummy_profiles/profile-pic-6.png');
+const UserProfile7 = require('./images/dummy_profiles/profile-pic-7.png');
+const UserProfile8 = require('./images/dummy_profiles/profile-pic-8.png');
+const UserProfile9 = require('./images/dummy_profiles/profile-pic-9.png');
+const UserProfile10 = require('./images/dummy_profiles/profile-pic-10.png');
+
+export default {
+    loginBackground,
+    profile,
+    recipe,
+    spagetti,
+    satay,
+    laksa,
+    nasiLemak,
+
+    UserProfile1,
+    UserProfile2,
+    UserProfile3,
+    UserProfile4,
+    UserProfile5,
+    UserProfile6,
+    UserProfile7,
+    UserProfile8,
+    UserProfile9,
+    UserProfile10,
+};
